a215d0005346bf1b94cc4b6fcb82b80bb1a2a25e
Charles <chaseuelt@gmail.com>
2024-04-29 12:34:10 -0500

    added pane styling to main page

--- view/src/components/main/Main.tsx
+++ view/src/components/main/Main.tsx
@@ -3,9 +3,11 @@ import "./Main.css"
 
 const Main = () => {
   return (
-    <SplitPane split="vertical" >
-      <Pane minSize="10%" >You can use a Pane component</Pane>
-      <Pane minSize="10%" >
+    <SplitPane split="vertical" defaultSize="50%">
+      <Pane >
+        Using a Pane allows you to specify any constraints directly
+      </Pane>
+      <Pane >
         Using a Pane allows you to specify any constraints directly
       </Pane>
     </SplitPane>
