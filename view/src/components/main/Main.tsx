452d1d04a56990117e9c341e22e0af884cf260b3
Charles Ueltschey <chaseuelt@gmail.com>
2024-05-02 15:53:28 -0500

    added squares interface and props

--- view/src/components/main/Main.tsx
+++ view/src/components/main/Main.tsx
@@ -4,10 +4,31 @@ import Squares from "./Squares.tsx"
 import Edit from "./Edit.tsx"
 
 const Main = () => {
+  const [selected, setSelected] = useState(0);
+  const [squares, setSquares] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/data'); // Assuming your local endpoint is '/api/data'
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const jsonData = await response.json();
+        setSquares(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <SplitPane split="vertical" defaultSize="50%">
       <Pane >
-        <Squares />
+        <Squares squares={squares} setSelected={(index : number) => setSelected(index)} selected={selected} />
       </Pane>
       <Pane >
         <Edit />
--- view/src/components/main/Squares.tsx
+++ view/src/components/main/Squares.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
-const Squares = () => {
+interface Square {
+  
+}
+
+interface Props: {
+  squares : Square[];
+  selected : number;
+  setSelected: (index) => void;
+}
+
+const Squares = ({squares, selected, setSelected}:Props) => {
   return (
     <div>Squares</div>
   )
